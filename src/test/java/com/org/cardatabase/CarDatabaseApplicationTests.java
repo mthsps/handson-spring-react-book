783a981e7b107e9e021ebe44f2ec8a2609d33d24
Matheus <dmatheusps@gmail.com>
2022-05-03 00:01:38 -0300

    Chapter 5: Creating unit tests

--- src/test/java/com/org/cardatabase/CarDatabaseApplicationTests.java
+++ src/test/java/com/org/cardatabase/CarDatabaseApplicationTests.java
@@ -1,13 +1,23 @@
 package com.org.cardatabase;
 
+import com.org.cardatabase.controller.CarController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CarDatabaseApplicationTests {
+	@Autowired
+	private CarController controller;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
